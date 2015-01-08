8a3e5ef0758286e6a230d43e253a4072e57d8ebe
Sergey Panov <gostroy@gmail.com>
2015-01-08 14:10:20 +0200

    generate Web-Service XSD file (формат представления данных)

---
+++ test_soap_project/spring_in_action_ch15/src/main/resources/ua/org/gostroy/spring_in_action_ch15/command.cmd
@@ -0,0 +1,2 @@
+cd C:\Users\Panov Sergey\IdeaProjects\soap\test_soap_project\spring_in_action_ch15\src\main\resources\ua\org\gostroy\spring_in_action_ch15
+java -jar c:\java\trang\trang.jar request1.xml response1.xml PokerTypes.xsd
